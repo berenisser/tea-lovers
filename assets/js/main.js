fa7b45c20c471068f572a0d40fe88fed5adb881a
berenisser <berenisser@gmail.com>
2017-05-18 22:50:14 -0400

    Validaciones listas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,44 @@
+//Llamamos al boton de suscripcion
 var botonSuscribirse = document.getElementById("suscribirse");
-var showInputs = document.querySelector(".todos-inputs");
-botonSuscribirse.addEventListener("click", display);
+botonSuscribirse.addEventListener("click", confirmar);
 
-function display(){
-	showInputs.classList.toggle("show");
+function confirmar(){
+	validarNombre();
+	validarEmail();
+	validarDireccion();
+	validarSeleccion();
 }
 
-function confirm(){
-	alert("Muchas gracias por suscribirte");
+function validarNombre(){
+	var nombre = document.getElementById("nombre").value;
+	if (!(/^[a-zA-Z]{2,30}$/).test(nombre)){
+		alert("Error: Debe llenar todos los campos");
+	}
+}
+
+function validarEmail(){
+	var email = document.getElementById("email").value;
+	if(!(/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/).test(email)){
+		alert("Error: Debe escribir su email correctamente")
+	} 
+}
+
+function validarDireccion(){
+	var direccion = document.getElementById("direccion").value;
+	if(!(/^[a-zA-Z0-9]/).test(direccion)){
+		alert("Debe escribir su direccion correctamente");
+	}
+}
+
+function validarSeleccion(){
+	var seleccion = document.getElementById("selecciones");
+		if (seleccion.value == 0){
+			alert("Debe seleccionar una opción de té");
+		}else{
+			document.getElementById("nombre").value ="";
+			document.getElementById("email").value = "";
+			document.getElementById("direccion").value = "";
+			document.getElementById("selecciones").value = "0";	
+			alert("Muchas gracias por suscribirte");
+		}
 }
